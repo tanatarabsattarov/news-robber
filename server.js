c9356bca3d1b8b37f5254a8090ee2d341a62e8d5
Tanatar Absattarov <Tanatar@Tanatars-MacBook-Air.local>
2019-08-17 21:26:14 -0400

    final upload

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news_robber";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
 
 app.get("/scrape", function (req, res) {
     axios.get("https://news.ycombinator.com/").then(function (response) {
