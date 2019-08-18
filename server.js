bb0d71d02f6f5bb806b0fc605adb1cd0d92ce3ee
Tanatar Absattarov <Tanatar@Tanatars-MacBook-Air.local>
2019-08-17 22:37:47 -0400

    errors

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var db = require("./models");
-var PORT = 3000;
+// var PORT = 3000;
 var app = express();
 
 app.use(logger("dev"));
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news_robber";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.get("/scrape", function (req, res) {
@@ -74,6 +74,6 @@ app.post("/articles/:id", function(req, res) {
   });
   
 
-app.listen(PORT, function () {
-    console.log("App running on port " + PORT + "!");
+app.listen(process.env.PORT || 3000,  function () {
+    console.log("App running on port " + 3000 + "!");
 });
